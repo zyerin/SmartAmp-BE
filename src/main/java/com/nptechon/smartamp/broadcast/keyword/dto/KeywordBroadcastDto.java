5d1c6398a8923a67ef591a3f2a13449a34dfd4d0
zyerin <zyerin5974@gmail.com>
2026-02-08 22:23:35 +0900

    feat: 키워드 방송 요청을 위한 KeywordBroadcastDto 추가
    - 키워드 방송 대상 앰프 ID와 방송 내용 필드 정의
    - 키워드 기반 방송 API DTO 구성

---
+++ src/main/java/com/nptechon/smartamp/broadcast/keyword/dto/KeywordBroadcastDto.java
@@ -0,0 +1,11 @@
+package com.nptechon.smartamp.broadcast.keyword.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class KeywordBroadcastDto {
+    private int ampId;
+    private String content;
+}
